13d355036c149e389fced4a7f2e2833f5db7345a
gshuenck <gilmar_shuenck@hotmail.com>
2024-09-27 19:59:39 -0300

    jogo

---
+++ Detona-ralph-aula/scripts/engine.js
@@ -0,0 +1,29 @@
+// alert("Hello World");
+
+const state = {
+    view: {
+        squres: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector(#time-left),
+        score: document.querySelector("score"),
+    },
+    values:{},
+};
+
+values:{},
+};
+
+function randomSquare(){
+    state.view.squares.forEach((square)) =
+}
+
+function addListenerHitbox() {
+    state.view.square.foEach((square)) =>{
+        if(square.id ==)
+    }
+}
+
+function initialize() {}
+
+initialize();
+
